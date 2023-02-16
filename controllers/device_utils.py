463b874c8c60feff2cc7aa52856da0d392728927
dyadyaJora <ja_massa97@mail.ru>
2023-02-16 22:55:18 +0300

    add gpiozero

---
+++ controllers/device_utils.py
@@ -0,0 +1,16 @@
+from gpiozero import OutputDevice, InputDevice
+
+
+class DigitDisplayUtils:
+    def __init__(self):
+        pass
+
+
+class LEDMultiCharDisplayWithShifter(OutputDevice):
+    def __init__(self):
+        super().__init__()
+
+
+class IRReceiver(InputDevice):
+    def __init__(self):
+        super(IRReceiver, self).__init__()
